d4454449b2b33b948caeac6cad8879862625074b
Alexandr Tikhonov <t9101029991@gmail.com>
2016-03-03 18:33:06 +0300

    base template main page

---
+++ js/OpenPopup.js
@@ -0,0 +1,25 @@
+﻿
+function jsfOpenPopup(sUrl, iWidth, iHeight, sProps)
+{
+	var winPopup = window.open(sUrl, 'PrintPopup', 'width=' + iWidth + ',height=' + iHeight + ',' + sProps);
+	winPopup.focus();
+//	return winPopup;
+}
+
+
+function clickButton(strID, event) {
+	var pButton = document.getElementById(strID);
+	if (event && event.which) {
+		if (event.which == 13) {
+			pButton.click();
+			return false;
+		}
+	} else if (window.event && window.event.keyCode) {
+		if (event.keyCode == 13) {
+			event.returnValue = false;
+			event.cancel = true;
+			pButton.click();
+			return false;
+		}
+	}
+}
