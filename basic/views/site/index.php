7168e4be4e697436e5492d550e6966f70ee09660
Alexandr Tikhonov <t9101029991@gmail.com>
2016-03-04 18:11:33 +0300

    template: main_page

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -7,93 +7,89 @@ $this->title = 'Сенерджи';
 
 ?>
 
-<div><h1>ООО "Сенарджи-НН"</h1></div>
-                    <div>
-                    <table cellspacing=0 cellpadding=0 width=740 border=0>
-    <tbody>
-        <tr>
-            <td style="height:55px;width:408px">Компания "Сэнарджи®" является разработчиком и производителем систем наружной теплоизоляции фасадов зданий "Сэнарджи® МвС" и "Сэнарджи® ПпС-3". Системы утепления "Сэнарджи®" полностью сертифицированы и имеют Техническое свидетельство Росстроя России.</td>
-            <td rowspan=2 align=center>
-            <p style="width:110px"><a href="/en/Canada/Pages/default.aspx"><img style="border-top:0px solid;border-right:0px solid;border-bottom:0px solid;border-left:0px solid" alt="Senergy in Canada" src="http://www.senergy.basf.com/SiteCollectionImages/basf-senergy-canada-thumbnail.jpg"></a><br>
-            <img style="border-top:0px solid;border-right:0px solid;vertical-align:top;border-bottom:0px solid;border-left:0px solid" alt="Senergy Communities" src="http://www.senergy.basf.com/en/PublishingImages/communities.jpg"><br>
-            <a href="/en/architects/Pages/default.aspx"><img style="border-top:0px solid;border-right:0px solid;vertical-align:top;border-bottom:0px solid;border-left:0px solid" alt="Link to Architect Community" src="http://www.senergy.basf.com/en/PublishingImages/Communities_images_architects.jpg"></a><br>
-            <a href="/en/contractors/Pages/default.aspx"><img style="border-top:0px solid;border-right:0px solid;vertical-align:top;border-bottom:0px solid;border-left:0px solid" alt="Link to Contractor Community" src="http://www.senergy.basf.com/en/PublishingImages/Communities_images_contractor.jpg"><br>
-            </a><a href="/en/developer/Pages/default.aspx"><img style="border-top:0px solid;border-right:0px solid;vertical-align:top;border-bottom:0px solid;border-left:0px solid" alt="Link to Owner Community" src="http://www.senergy.basf.com/en/PublishingImages/Communities_images_owner.jpg"></a></p>
-            </td>
-            <td style="width:194px;background-color:#004a96" valign=top rowspan=2><span class="whatsnews_title">What's New &gt;&gt;</span><br>
-            <br>
-            <a href="/en/platinum-ci-systems/Pages/default.aspx" class="whatsnew_topbox_bold">• Continuous Insulation</a><br>
-            <a href="/en/platinum-ci-systems/Pages/default.aspx" class="whatsnew_topbox">Platinum CI Wall Systems</a><br>
-            <br>
-            <a href="/en/products/chroma-finish/Pages/default.aspx" class="whatsnew_topbox_bold">• Enhanced Anti-Soiling</a><br>
-            <div class="indent_specialty"><a href="/SiteCollectionDocuments/Technical%20Bulletins/BASF-senergy-senerflex-tersus-finish.pdf" class="whatsnew_topbox_standardlink" target="_blank">» Tersus Finish</a></div>
-            <div class="indent_specialty"><a href="/en/products/Coatings/Pages/Tersus-Color-Coat.aspx" class="whatsnew_topbox_standardlink" target="_blank">» Tersus Color Coat</a></div>
-            <br>
-            <div class="first_line_indent"><a href="http://www.wallsystems.basf.com/SiteCollectionDocuments/stucco/BASF-product-bulletin-DiamondShield.pdf" class="whatsnew_topbox_standardlink">• DiamondShield</a></div>
-            <br>
-            <div class="first_line_indent"><a href="/EN/TECHNICAL_SUPPORT/Pages/ContinuingEducation.aspx" class="whatsnew_topbox_standardlink">• Earn HSW &amp; Sustainability credits - Online AIA/CES course High Performance EIFS</a></div>
-            <br>
-            <div class="first_line_indent"><a href="/SiteCollectionDocuments/Brochure_Eco-efficiency.pdf" class="whatsnew_topbox_standardlink">• Our Eco-efficiency Brochure</a></div>
-            <br>
-            <div class="first_line_indent"><a href="/en/spac/Pages/default.aspx" class="whatsnew_topbox_standardlink">• Submittal Assembly Center</a></div>
-            <br>
-            <div class="first_line_indent"><a href="http://www.wallsystems.basf.com/SiteCollectionDocuments/sustainability/BASF-LEED-Request-Form.pdf" class="whatsnew_topbox_standardlink" target="_blank">• BASF LEED Request Form</a></div>
-            <br>
-            <a href="/en/products/chroma-finish/Pages/default.aspx" class="whatsnew_topbox_bold">• Specialty Finishes</a>
-            <div class="indent_specialty"></div>
-            <div class="indent_specialty"><a href="/en/products/chroma-finish/Pages/default.aspx" class="whatsnew_topbox_standardlink">» ChromaFinish</a></div>
-            <div class="indent_specialty"><a href="/SiteCollectionDocuments/Technical%20Bulletins/Senergy_Tech_Bulletin_Senerbrick.pdf" class="whatsnew_topbox_standardlink">» Senerbrick</a></div>
-            <div class="indent_specialty"><a href="/en/products/TexturedFinishes/SpecialtyFinishes/Pages/MetallicFinish.aspx" class="whatsnew_topbox_standardlink">» Metallic Finish</a></div>
-            <div class="indent_specialty"><a href="/en/products/Coatings/Pages/AnticoGlaze.aspx" class="whatsnew_topbox_standardlink">» AnticoGlaze</a></div>
-            </td>
-        </tr>
-        <tr>
-            <td><a href="/en/platinum-ci-systems/Pages/default.aspx" target="_blank"><img style="border-top:0px solid;height:320px;border-right:0px solid;width:408px;vertical-align:text-bottom;border-bottom:0px solid;border-left:0px solid" alt="Platinum Continuously Insulated Wall Systems" src="http://www.senergy.basf.com/SiteCollectionImages/BASF-senergy-start-page/BASF-platinum-ci-and-ci-stucco-start-page.jpg" usemap="#rade_img_map_1313084870530"></a></td>
-        </tr>
-    </tbody>
-</table>
-<table cellspacing=0 cellpadding=0 width=740 border=0>
-    <tbody>
-        <tr>
-            <td style="height:14px"><br>
-            </td>
-        </tr>
-    </tbody>
-</table>
-<table height=300 cellspacing=0 cellpadding=0 width=740 border=0>
-    <tbody>
-        <tr>
-            <td><img style="border-top:0px solid;height:146px;border-right:0px solid;width:114px;border-bottom:0px solid;border-left:0px solid" alt="Senergy Literature" src="http://www.senergy.basf.com/en/PublishingImages/literature.jpg" usemap="#rade_img_map_1406919925891"></td>
-            <td style="width:14px" rowspan=3><br>
-            </td>
-            <td valign=top rowspan=3 align=left><a href="/en/colors_and_textures/gallery/Pages/default.aspx#01"><img style="border-top:0px solid;height:300px;border-right:0px solid;width:383px;border-bottom:0px solid;border-left:0px solid" alt="Link to Photo Gallery" src="http://www.senergy.basf.com/SiteCollectionImages/BASF-webinar-button-PCI-AWRB-senergy.jpg" usemap="#rade_img_map_1405348603443"></a></td>
-            <td style="width:14px" rowspan=3><br>
-            </td>
-            <td style="background-color:rgb(223,241,251)"><a href="/en/distributors/Pages/default.aspx"><img style="border-top:0px solid;height:146px;border-right:0px solid;border-bottom:0px solid;border-left:0px solid" alt="Senergy Distributors" src="http://www.senergy.basf.com/en/PublishingImages/distributor.jpg"></a></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td style="width:194px"><br>
-            </td>
-        </tr>
-        <tr>
-            <td><img style="border-top:0px solid;height:140px;border-right:0px solid;border-bottom:0px solid;border-left:0px solid" alt="Senergy Wall Systems" src="http://www.senergy.basf.com/en/PublishingImages/wall_systems.jpg" usemap="#rade_img_map_ctl00_PlaceHolderMain_RadHtmlField1_ctl00_1"></td>
-            <td style="width:194px"><a href="/en/about_senergy/our_sustainability/Pages/default.aspx" target="_blank"><img style="border-top:0px solid;height:140px;border-right:0px solid;border-bottom:0px solid;border-left:0px solid" alt="Senergy and Sustainability" src="http://www.senergy.basf.com/en/PublishingImages/sustain.jpg"></a></td>
-        </tr>
-    </tbody>
-</table>
-<br>
-<p> </p>
-<p><map id="rade_img_map_ctl00_PlaceHolderMain_RadHtmlField1_ctl00_1" name="rade_img_map_ctl00_PlaceHolderMain_RadHtmlField1_ctl00_1">
-<area shape=rect coords="4,2,85,19" href="/en/wall_systems/Pages/default.aspx" shape=RECT coords="4,2,85,19">
-<area shape=rect coords="4,58,109,133" href="/en/wall_systems/Pages/default.aspx" shape=RECT coords="4,58,109,133">
-<area shape=rect coords="4,40,66,57" href="/en/wall_systems/Sentry_Stucco/Pages/default.aspx" shape=RECT coords="4,40,66,57">
-<area shape=rect coords="5,23,47,39" href="/en/wall_systems/pb/Pages/default.aspx" shape=RECT coords="5,23,47,39"></map><map id="rade_img_map_1405348603443" name="rade_img_map_1405348603443">
-<area shape=rect coords="4,0,385,303" href="/en/colors_and_textures/gallery/Pages/default.aspx" shape=RECT coords="4,0,385,303"></map><map id="rade_img_map_1406919925891" name="rade_img_map_1406919925891">
-<area shape=rect coords="3,1,65,17" href="/en/literature/Pages/default.aspx" shape=RECT coords="3,1,65,17">
-<area shape=rect coords="14,96,112,144" href="/en/literature/Pages/default.aspx" shape=RECT coords="14,96,112,144">
-<area shape=rect coords="2,21,68,41" href="/en/literature/product_bulletins/Pages/default.aspx" shape=RECT coords="2,21,68,41">
-<area shape=rect coords="2,42,67,57" href="/en/literature/details/Pages/default.aspx" shape=RECT coords="2,42,67,57">
-<area shape=rect coords="2,72,50,88" href="/en/literature/sds/Pages/default.aspx" shape=RECT coords="2,72,50,88">
-<area shape=rect coords="3,57,83,72" href="/en/literature/tech_bulletins/Pages/default.aspx#08" shape=RECT coords="3,57,83,72"></map></p>
+<div class='col-md-12'><h1>ООО "Сенарджи-НН"</h1></div>
+                    
+<div class="col-md-8">
+    <div class="alert alert-info">
+        <span class='glyphicon glyphicon-info-sign'></span> Компания "Сэнарджи®" является разработчиком и производителем систем наружной теплоизоляции фасадов зданий "Сэнарджи® МвС" и "Сэнарджи® ПпС-3". Системы утепления "Сэнарджи®" полностью сертифицированы и имеют Техническое свидетельство Росстроя России.
+    </div>
+    <div class="main_banner_txt">
+        Системы утепления
+        <br>
+        <?= Html::a('Подробнее »',['site/view', 'guid'=>'sistemy-uteplenia']);?>
+    </div>
+    <img src="/images/senardgy_banner.jpg" class='thumbnail img-responsive'>
 </div>
+
+<div class="col-md-4">
+    <div class="list-group">
+
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Заголовок блока</h4>
+        <p class="list-group-item-text">Краткое описание раздела</p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Заголовок блока</h4>
+        <p class="list-group-item-text">Краткое описание раздела</p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Заголовок блока</h4>
+        <p class="list-group-item-text">Краткое описание раздела</p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Заголовок блока</h4>
+        <p class="list-group-item-text">Краткое описание раздела</p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Заголовок блока</h4>
+        <p class="list-group-item-text">Краткое описание раздела</p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Заголовок блока</h4>
+        <p class="list-group-item-text">Краткое описание раздела</p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Заголовок блока</h4>
+        <p class="list-group-item-text">Краткое описание раздела</p>
+      </a>
+      <a href="#" class="list-group-item">
+        <h4 class="list-group-item-heading">Заголовок блока</h4>
+        <p class="list-group-item-text">Краткое описание раздела</p>
+      </a>
+
+    </div>
+</div>
+
+<div class="col-md-12">
+    <img src='https://placeholdit.imgix.net/~text?txtsize=50&txt=баннер&w=690&h=200' class='img-responsive'>
+</div>
+
+
+<div class="col-md-4" style="margin-top:20px; text-align:center">
+          <img class="img-circle" src="https://placehold.it/200x200" alt="Generic placeholder image" width="140" height="140">
+          <h2>Заголовок</h2>
+          <p>Краткое описание раздела, которое в общих чертах дает пользователю понимание, что находится в самом разделе и есть ли там нужная ему информация</p>
+          <p><a class="btn btn-default" href="#" role="button">Подробнее »</a></p>
+</div>
+
+<div class="col-md-4" style="margin-top:20px; text-align:center">
+          <img class="img-circle" src="https://placehold.it/200x200" alt="Generic placeholder image" width="140" height="140">
+          <h2>Заголовок</h2>
+          <p>Краткое описание раздела, которое в общих чертах дает пользователю понимание, что находится в самом разделе и есть ли там нужная ему информация</p>
+          <p><a class="btn btn-default" href="#" role="button">Подробнее »</a></p>
+</div>
+
+<div class="col-md-4" style="margin-top:20px; text-align:center">
+          <img class="img-circle" src="https://placehold.it/200x200" alt="Generic placeholder image" width="140" height="140">
+          <h2>Заголовок</h2>
+          <p>Краткое описание раздела, которое в общих чертах дает пользователю понимание, что находится в самом разделе и есть ли там нужная ему информация</p>
+          <p><a class="btn btn-default" href="#" role="button">Подробнее »</a></p>
+</div>
+
+<div class="row">
+<div style='height:100px;width:100%'></div>
+</div>
+
+
+
+             
