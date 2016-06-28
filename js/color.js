1343ac2e06d75054ce33b09e8e4cf2ecbec1b472
Alexandr Tikhonov <t9101029991@gmail.com>
2016-06-28 11:41:22 +0300

    add colors number

--- js/color.js
+++ js/color.js
@@ -1,19 +1,274 @@
+var colors = {
+    '3025': 'Paprika',
+    '3029': 'Creme Brule',
+    '3035': 'Strawberry',
+    '3037': 'Mariposa',
+    '3053': 'Tawny',
+    '3074': 'Coriander',
+    '528': 'Sun Glow',
+    '3026': 'Clay Pot',
+    '3031': 'Ground Mace',
+    '3030': 'Solitude',
+    '3028': 'Vintage',
+    '3038': 'Scarborough',
+    '3066': 'Bambi',
+    '3067': 'Egg Cream',
+    '545': 'Dusty Peach',
+    '3027': 'Mannequin',
+    '3036': 'Daiquiri',
+    '3033': 'Peacherine',
+    '3039': 'Coral',
+    '3049': 'Artisan',
+    '3044': 'Ramie',
+    '543': 'Slate Rose',
+    '3059': 'Mosque',
+    '547': 'Blush',
+    '3041': 'Firenze',
+    '600': 'Silverado',
+    '3047': 'Tortilla',
+    '3080': 'Ecru',
+    '3043': 'Pueblo',
+    '3032': 'Melon',
+    '3042': 'Nirvana',
+    '3048': 'Lotus',
+    '3054': 'Sonata',
+    '3057': 'Luna',
+    '3051': 'Windsong',
+    '614': 'Clay',
+    '3073': 'Hickory',
+    '612': 'Seashell',
+    '549': 'Pink Ash',
+    '3103': 'Sandstorm',
+    '3063': 'Frappe',
+    '3062': 'Angora',
+    '620': 'Ash Brown',
+    '618': 'Hazelnut',
+    '3052': 'Cardamon',
+    '3064': 'Rawhide',
+    '3060': 'Cafe au Lait',
+    '3061': 'Dune',
+    '3055': 'Shearling',
+    '3069': 'Sepia',
+    '3070': 'Brunet',
+    '3071': 'Java',
+    '3072': 'Bulgur',
+    '433': 'Aztec Gold',
+    '3065': 'Peanut',
+    '361': 'Champagne',
+    '3086': 'Lion’s Mane',
+    '3081': 'Empire',
+    '3076': 'Umber',
+    '3077': 'Ocha',
+    '3082': 'Cumin',
+    '3078': 'Gold Ochre',
+    '3079': 'Bay Leaf',
+    '606': 'Glacier',
+    '3089': 'Gauze',
+    '3090': 'Honeysuckle',
+    '440': 'Custard',
+    '3085': 'Meringue',
+    '3097': 'Saddlesoap',
+    '3083': 'Flax',
+    '3088': 'Cat’s Eye',
+    '3046': 'Finesse',
+    '3084': 'Egg Nog',
+    '3075': 'Parsnip',
+    '3034': 'Muslin',
+    '601': 'Dynasty',
+    '3091': 'Sprite',
+    '3087': 'Mint Aspic',
+    '3045': 'Edelweiss',
+    '3068': 'Birch',
+    '3050': 'Nougat',
+    '410': 'Snow',
+    '3094': 'Harrier',
+    '3099': 'Brume',
+    '869': 'London Fog',
+    '354': 'Stark White',
+    '330': 'Ultra - White',
+    '3058': 'Nightingale',
+    '3096': 'Orchid',
+    '3092': 'New Frond',
+    '3095': 'Mint Tea',
+    '3056': 'Papyrus',
+    '342': 'Parchment',
+    '3100': 'Biscuit',
+    '425': 'Salt White',
+    '3101': 'Vibrato',
+    '752': 'Dawn Grey',
+    '751': 'Morning Mist',
+    '3040': 'Jute',
+    '431': 'Almond',
+    '427': 'Oatmeal',
+    '610': 'Porridge',
+    '3098': 'Crescent',
+    '3104': 'Walden',
+    '3102': 'Revere',
+    '3105': 'Sandman',
+    '3106': 'Concord',
+    '3107': 'Sage',
+    '3108': 'Badger',
+    '329': 'Putty Grey',
+    '875': 'Grey Cloud',
+    '877': 'Slate Grey',
+}
+
+var colors2 = {
+    'Paprika' : '3025',
+    'Creme Brule' : '3029',
+    'Strawberry' : '3035',
+    'Mariposa' : '3037',
+    'Tawny' : '3053',
+    'Coriander' : '3074',
+    'Sun Glow' : '528',
+    'Clay Pot' : '3026',
+    'Ground Mace' : '3031',
+    'Solitude' : '3030',
+    'Vintage' : '3028',
+    'Scarborough' : '3038',
+    'Bambi' : '3066',
+    'Egg Cream' : '3067',
+    'Dusty Peach' : '545',
+    'Mannequin' : '3027',
+    'Daiquiri' : '3036',
+    'Peacherine' : '3033',
+    'Coral' : '3039',
+    'Artisan' : '3049',
+    'Ramie' : '3044',
+    'Slate Rose' : '543',
+    'Mosque' : '3059',
+    'Blush' : '547',
+    'Firenze' : '3041',
+    'Silverado' : '600',
+    'Tortilla' : '3047',
+    'Ecru' : '3080',
+    'Pueblo' : '3043',
+    'Melon' : '3032',
+    'Nirvana' : '3042',
+    'Lotus' : '3048',
+    'Sonata' : '3054',
+    'Luna' : '3057',
+    'Windsong' : '3051',
+    'Clay' : '614',
+    'Hickory' : '3073',
+    'Seashell' : '612',
+    'Pink Ash' : '549',
+    'Sandstorm' : '3103',
+    'Frappe' : '3063',
+    'Angora' : '3062',
+    'Ash Brown' : '620',
+    'Hazelnut' : '618',
+    'Cardamon' : '3052',
+    'Rawhide' : '3064',
+    'Cafe au Lait' : '3060',
+    'Dune' : '3061',
+    'Shearling' : '3055',
+    'Sepia' : '3069',
+    'Brunet' : '3070',
+    'Java' : '3071',
+    'Bulgur' : '3072',
+    'Aztec Gold' : '433',
+    'Peanut' : '3065',
+    'Champagne' : '361',
+    'Lions Mane' : '3086',
+    'Empire' : '3081',
+    'Umber' : '3076',
+    'Ocha' : '3077',
+    'Cumin' : '3082',
+    'Gold Ochre' : '3078',
+    'Bay Leaf' : '3079',
+    'Glacier' : '606',
+    'Gauze' : '3089',
+    'Honeysuckle' : '3090',
+    'Custard' : '440',
+    'Meringue' : '3085',
+    'Saddlesoap' : '3097',
+    'Flax' : '3083',
+    'Cats Eye' : '3088',
+    'Finesse' : '3046',
+    'Egg Nog' : '3084',
+    'Parsnip' : '3075',
+    'Muslin' : '3034',
+    'Dynasty' : '601',
+    'Sprite' : '3091',
+    'Mint Aspic' : '3087',
+    'Edelweiss' : '3045',
+    'Birch' : '3068',
+    'Nougat' : '3050',
+    'Snow' : '410',
+    'Harrier' : '3094',
+    'Brume' : '3099',
+    'London Fog' : '869',
+    'Stark White' : '354',
+    'Ultra-White' : '330',
+    'Nightingale' : '3058',
+    'Orchid' : '3096',
+    'New Frond' : '3092',
+    'Mint Tea' : '3095',
+    'Papyrus' : '3056',
+    'Parchment' : '342',
+    'Biscuit' : '3100',
+    'Salt White' : '425',
+    'Vibrato' : '3101',
+    'Dawn Grey' : '752',
+    'Morning Mist' : '751',
+    'Jute' : '3040',
+    'Almond' : '431',
+    'Oatmeal' : '427',
+    'Porridge' : '610',
+    'Crescent' : '3098',
+    'Walden' : '3104',
+    'Revere' : '3102',
+    'Sandman' : '3105',
+    'Concord' : '3106',
+    'Sage' : '3107',
+    'Badger' : '3108',
+    'Putty Grey' : '329',
+    'Grey Cloud' : '875',
+    'Slate Grey' : '877',
+    'Greenbrier' : '609',
+    'Bluet' : '605',
+    'Glade' : '608',
+    'Truro Blue': '604',
+    'Gem' : '607',
+    'Atlantis' : '603',
+    'Crystal Blue' : '602',
+    'Robin Egg' : '611',
+    'Seamist' : '755',
+    'Seattle Mist' : '3093',
+    'Grey Ash' : '3109',
+    'Thundermist' : '758',
+    'Whale Grey' : '872',
+    'Magnum' : '757',
+    'Storm Grey' : '870',
+    'Charcoal' : '880',
+}
+
 window.onload = function() {
+
+    // $(".ColorComparisonSmallText").each(function() {
+    //     currColor = $(this).text().replace("<strong></strong>").trim();
+    //     colorNumber = colors2[currColor];
+    //     colorNumber ? $(this).append(" "+ colorNumber) : false;
+    //     console.log(colorNumber);
+
+    // });
+    // console.log(colors2);
     s = Snap("#svg");
 
     s.image('../images/senardgy_color_selector.jpg', 0, 0, 400, 524);
-    
+
     wall1 = s.path(["M102 289l55 0 0 1 1 2 0 2 0 4 0 1 9 0 0 -3 0 -3 0 -2 0 -2 14 0 1 2 0 1 0 3 0 3 0 1 9 0 0 -3 0 -2 0 -2 0 -2 0 -1 15 0 0 2 0 2 0 2 0 3 1 1 8 0 0 -3 0 -2 1 -3 0 -2 14 0 0 2 0 3 1 2 0 2 0 1 8 0 1 -3 0 -3 0 -3 0 -1 18 0 38 0 0 96 -4 0 -1 0 0 -1 -2 -1 0 0 0 -1 -2 0 0 1 0 2 -1 0 -1 0 -1 0 0 -72 -48 0 0 75 0 6 -14 10 0 0 0 -91 -47 0 0 90 -15 0 0 -90 -46 0 0 90 -12 0 0 -114zm130 40l1 0 0 3 0 3 1 2 1 1 1 2 0 2 0 3 -1 1 0 2 -1 2 -1 1 0 2 -1 0 -1 -2 -1 -1 0 -3 -1 -3 0 -1 0 -1 0 -1 0 -2 2 -2 0 -2 1 -2 0 -3 0 -1zm57 0l1 0 0 2 0 4 1 1 1 2 0 1 1 1 1 1 0 1 -1 1 0 1 -1 2 0 1 1 1 -2 0 0 2 0 1 -1 1 0 1 0 1 -1 -1 0 -1 -1 -2 -1 -1 0 -2 0 -3 -1 -1 0 -1 0 -1 0 -1 1 -1 1 -2 0 -3 0 -3 1 -1 0 -1z"]);
     wall2 = s.path(["M102 178l16 0 0 0 39 -1 0 -4 84 -1 1 5 38 0 1 0 15 0 0 95 -12 0 -1 0 -38 0 -1 -2 -1 -1 -89 0 -1 1 0 2 -37 0 -2 0 0 1 -12 0 0 -95zm147 8l23 0 0 4 -2 0 -1 67 -18 0 0 -67 -2 0 0 -4zm-84 -3l66 0 0 2 0 4 0 66 -66 -1 0 -71zm-39 4l23 0 0 3 -2 0 -1 68 -19 0 0 -66 0 -2 -1 0 0 -3z"]);
     wall3 = s.polygon(["101,150 102,132 101,131 100,129 100,127 100,126 102,125 115,125 115,125 157,124 159,121 160,120 159,116 157,116 157,86 183,86 241,86 241,115 239,116 239,118 239,119 239,120 240,122 241,124 242,125 281,124 283,124 283,125 298,125 298,128 297,130 297,132 296,133 296,148 250,148 250,143 231,143 231,95 167,95 167,143 226,143 149,144 149,149"]);
     wall = s.group(wall1, wall2, wall3);
-    
+
     roof = s.polygon(["136,50 255,50 259,60 262,65 269,76 277,88 284,101 290,109 294,112 298,116 241,115 240,85 244,86 246,80 246,76 246,74 248,71 248,68 248,66 191,66 151,67 150,68 150,70 151,74 151,77 152,83 153,84 153,86 154,88 154,89 157,89 157,116 99,116 101,116 106,112 112,102 117,93 123,80 128,69 133,57 136,53"]);
-    
+
     arhElem1 = s.path(["M100 403l13 0 1 -90 46 0 0 90 15 0 0 -90 47 0 0 90 14 0 0 -90 48 0 0 106 -183 -1 0 -2 -1 0 0 -3 1 1 0 -9 -1 0 0 -2zm174 10l0 -94 -29 0 -1 93 30 1zm-93 -94l34 0 0 82 -34 0 0 -82zm-62 0l35 0 0 82 -34 0 -1 -82z"]);
-    arhElem2 = s.polygon(["300,272 300,274 299,275 299,277 297,277 298,283 297,283 297,289 240,289 240,293 239,296 239,299 231,298 231,293 230,292 230,290 229,289 216,289 216,291 215,294 215,296 215,299 206,299 206,297 206,294 206,291 206,289 206,289 192,289 191,289 191,291 191,294 191,297 191,299 182,299 182,296 182,294 182,292 182,290 181,289 167,289 167,292 167,294 167,297 167,299 158,299 158,296 158,292 158,290 157,288 101,289 101,288 101,283 101,282 100,278 100,277 99,275 98,273 114,273 114,272 116,272 117,272 138,272 152,272 153,272 153,270 154,269 158,269 183,269 209,269 242,269 244,269 245,270 245,272 272,272 283,272 284,272"]); 
-    arhElem3 = s.polygon(["93,150 148,149 149,144 250,143 250,148 305,148 305,152 304,153 298,166 297,168 297,170 297,177 260,177 242,177 241,172 192,172 157,173 157,178 118,178 118,178 102,178 102,175 98,165 "]); 
-    arhElem4 = s.polygon(["157,86 241,86 242,85 244,85 244,84 246,84 246,81 246,80 246,75 247,74 247,70 248,70 248,69 151,69 151,75 152,75 152,78 152,80 152,83 152,85 154,85 154,87 157,87 "]); 
+    arhElem2 = s.polygon(["300,272 300,274 299,275 299,277 297,277 298,283 297,283 297,289 240,289 240,293 239,296 239,299 231,298 231,293 230,292 230,290 229,289 216,289 216,291 215,294 215,296 215,299 206,299 206,297 206,294 206,291 206,289 206,289 192,289 191,289 191,291 191,294 191,297 191,299 182,299 182,296 182,294 182,292 182,290 181,289 167,289 167,292 167,294 167,297 167,299 158,299 158,296 158,292 158,290 157,288 101,289 101,288 101,283 101,282 100,278 100,277 99,275 98,273 114,273 114,272 116,272 117,272 138,272 152,272 153,272 153,270 154,269 158,269 183,269 209,269 242,269 244,269 245,270 245,272 272,272 283,272 284,272"]);
+    arhElem3 = s.polygon(["93,150 148,149 149,144 250,143 250,148 305,148 305,152 304,153 298,166 297,168 297,170 297,177 260,177 242,177 241,172 192,172 157,173 157,178 118,178 118,178 102,178 102,175 98,165 "]);
+    arhElem4 = s.polygon(["157,86 241,86 242,85 244,85 244,84 246,84 246,81 246,80 246,75 247,74 247,70 248,70 248,69 151,69 151,75 152,75 152,78 152,80 152,83 152,85 154,85 154,87 157,87 "]);
     arhElem = s.group(arhElem1, arhElem2, arhElem3, arhElem4);
 
     window1 = s.path(["M120 401l34 0 0 -82 -35 0 1 82zm20 -15l10 0 0 13 -10 0 0 -13zm-15 0l10 0 0 12 -10 0 0 -12zm0 -14l10 0 0 13 -10 0 0 -13zm0 -14l10 0 0 13 -10 0 0 -13zm0 -14l10 0 0 13 -10 0 0 -13zm15 28l10 0 0 13 -10 0 0 -13zm0 -14l10 0 0 13 -10 0 0 -13zm0 -14l10 0 0 13 -10 0 0 -13zm0 -20l11 0 0 14 -11 0 0 -14zm-15 0l10 0 0 14 -10 0 0 -14z"]);
@@ -24,58 +279,58 @@ window.onload = function() {
     window6 = s.path(["M167 95l64 0 1 48 -65 1 0 -49zm49 35l10 0 0 13 -10 0 0 -13zm-15 0l9 0 0 13 -9 0 0 -13zm-14 0l10 0 0 13 -10 0 0 -13zm-15 0l9 0 0 13 -9 0 0 -13zm44 -14l10 0 0 13 -10 0 0 -13zm-15 0l9 0 0 13 -9 0 0 -13zm-14 0l10 0 0 13 -10 0 0 -13zm-15 1l9 0 0 12 -9 0 0 -12zm44 -15l10 0 0 13 -10 0 0 -13zm-15 0l9 0 0 13 -9 0 0 -13zm-14 0l10 0 0 13 -10 0 0 -13zm-15 1l9 0 0 12 -9 1 0 -13z"]);
     window7 = s.path(["M244 319l31 0 -1 23 -29 0 -1 -23zm3 7l9 0 0 12 -9 0 0 -12zm15 0l9 0 0 11 -9 1 0 -12z"]);
     windows = s.group(window1, window2, window3, window4, window5, window6, window7);
-   // door = s.polygon(["244,342 274,342 274,413 244,413 "]);
+    // door = s.polygon(["244,342 274,342 274,413 244,413 "]);
 
-   tube1 = s.polygon(['265,67 271,40 289,40 289,42 288,44 288,47 288,106']);
-   tube2 = s.polygon(['107,40 125,40 129,66 108,109 108,48 107,45 ']);
-   tube = s.group(tube1, tube2);
+    tube1 = s.polygon(['265,67 271,40 289,40 289,42 288,44 288,47 288,106']);
+    tube2 = s.polygon(['107,40 125,40 129,66 108,109 108,48 107,45 ']);
+    tube = s.group(tube1, tube2);
 
     wall.addClass("wall");
     roof.addClass("roof");
     arhElem.addClass("arhElem");
     windows.addClass("windows");
     tube.addClass("tube");
-   // door.addClass("door");
+    // door.addClass("door");
 
-    wall.click(function(e){
+    wall.click(function(e) {
         $(".color-selector").removeClass("active");
         $(".btn-wall").addClass("active");
     });
 
-    roof.click(function(e){
+    roof.click(function(e) {
         $(".color-selector").removeClass("active");
         $(".btn-roof").addClass("active");
     });
 
-    arhElem.click(function(e){
+    arhElem.click(function(e) {
         $(".color-selector").removeClass("active");
         $(".btn-arhElem").addClass("active");
     });
 
-    windows.click(function(e){
+    windows.click(function(e) {
         $(".color-selector").removeClass("active");
         $(".btn-windows").addClass("active");
     });
 
-    tube.click(function(e){
+    tube.click(function(e) {
         $(".color-selector").removeClass("active");
         $(".btn-tube").addClass("active");
     });
 
-    $(".color-selector").click(function(){
-      $(".color-selector").removeClass("active");
-      $(this).addClass("active");
+    $(".color-selector").click(function() {
+        $(".color-selector").removeClass("active");
+        $(this).addClass("active");
     });
 
 
     $(".ColorComparisonSmallColor").click(function() {
         colorSelect = $(this).css('background-color');
-        rgbArray = colorSelect.substr(4, colorSelect.length-5).split(', ');
+        rgbArray = colorSelect.substr(4, colorSelect.length - 5).split(', ');
         hslColor = rgb2hsl(rgbArray);
         console.log(hslColor);
         elementForColor = $(".color-selector.active").attr("data");
-        $(".btn-"+elementForColor).css("border-bottom","10px solid "+colorSelect);
-        elementForColor ? $("."+elementForColor).css("fill", hslColor) : console.log("none");
+        $(".btn-" + elementForColor).css("border-bottom", "10px solid " + colorSelect);
+        elementForColor ? $("." + elementForColor).css("fill", hslColor) : console.log("none");
     });
 
 
@@ -83,53 +338,51 @@ window.onload = function() {
 
 
 /*
-* Converts an RGB color to HSL
-* Parameters
-*     rgbArr : 3-element array containing the RGB values
-*
-* Result : 3-element array containing the HSL values
-*
-*/
-function rgb2hsl(rgbArr){
+ * Converts an RGB color to HSL
+ * Parameters
+ *     rgbArr : 3-element array containing the RGB values
+ *
+ * Result : 3-element array containing the HSL values
+ *
+ */
+function rgb2hsl(rgbArr) {
     var r1 = rgbArr[0] / 255;
     var g1 = rgbArr[1] / 255;
     var b1 = rgbArr[2] / 255;
- 
-    var maxColor = Math.max(r1,g1,b1);
-    var minColor = Math.min(r1,g1,b1);
+
+    var maxColor = Math.max(r1, g1, b1);
+    var minColor = Math.min(r1, g1, b1);
     //Calculate L:
-    var L = (maxColor + minColor) / 2 ;
+    var L = (maxColor + minColor) / 2;
     var S = 0;
     var H = 0;
-    if(maxColor != minColor){
+    if (maxColor != minColor) {
         //Calculate S:
-        if(L < 0.5){
+        if (L < 0.5) {
             S = (maxColor - minColor) / (maxColor + minColor);
-        }else{
+        } else {
             S = (maxColor - minColor) / (2.0 - maxColor - minColor);
         }
         //Calculate H:
-        if(r1 == maxColor){
-            H = (g1-b1) / (maxColor - minColor);
-        }else if(g1 == maxColor){
+        if (r1 == maxColor) {
+            H = (g1 - b1) / (maxColor - minColor);
+        } else if (g1 == maxColor) {
             H = 2.0 + (b1 - r1) / (maxColor - minColor);
-        }else{
+        } else {
             H = 4.0 + (r1 - g1) / (maxColor - minColor);
         }
     }
- 
-    
-    
+
+
+
     H = H * 60;
-    if(H<0){
+    if (H < 0) {
         H += 360;
     }
     S = S * 100;
-    L =  L < 0.85 ? L * 100 : L * 100;
-    
+    L = L < 0.85 ? L * 100 : L * 100;
+
     var result = [H, S, L];
-   // return result;
-    return "hsl("+H+","+S+"%,"+L+"%)";
+    // return result;
+    return "hsl(" + H + "," + S + "%," + L + "%)";
 }
-    
-    
