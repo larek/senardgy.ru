f1c8de4a9a2eea677f83d3a4df64ab099cf7e6b5
Alexandr Tikhonov <t9101029991@gmail.com>
2016-06-29 14:52:39 +0300

    correct pinterest template

--- basic/views/site/vypolnennye-raboty.php
+++ basic/views/site/vypolnennye-raboty.php
@@ -25,5 +25,5 @@ $this->registerJsFile('//assets.pinterest.com/js/pinit.js', ['depends' => [\yii\
 </div>
 
 <div class="">
-<a data-pin-do="embedBoard" data-pin-lang="ru" data-pin-board-width="400" data-pin-scale-height="240" data-pin-scale-width="80" href="https://ru.pinterest.com/bril007/%25D0%25B0%25D0%25B0%25D0%25B0-%25D1%2581%25D1%258D%25D0%25BD%25D0%25B5%25D1%2580%25D0%25B4%25D0%25B6%25D0%25B8/"></a>
+<a data-pin-do="embedBoard" data-pin-lang="ru" data-pin-board-width="690" data-pin-scale-height="600" data-pin-scale-width="150" href="https://ru.pinterest.com/bril007/%D0%90%D0%90%D0%90-%D1%81%D1%8D%D0%BD%D0%B5%D1%80%D0%B4%D0%B6%D0%B8"></a>
 </div>
