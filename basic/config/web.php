e9a59fc6d5fcbcc2cc8d940ca8930fd964e23eb6
Aleksandr Tikhonov <t9101029991@gmail.com>
2018-01-25 19:50:04 +0300

    update smtp config

--- basic/config/web.php
+++ basic/config/web.php
@@ -42,7 +42,7 @@ $config = [
              'class' => 'Swift_SmtpTransport',
              'host' => 'smtp.yandex.ru',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
              'username' => 'saitom@yandex.ru',
-             'password' => 'avatarka9101029991',
+             'password' => 'LarekAvatar910',
              'port' => '587', // Port 25 is a very common port too
              'encryption' => 'tls', // It is often used, check your provider or mail server specs
          ],
