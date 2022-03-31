379888f90c26a2dc4f29a355ed84ac8b439e4f9c
Alexandr Tikhonov <t9101029991@gmail.com>
2022-03-31 17:14:43 +0300

    Update main.php

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -204,7 +204,7 @@ AppAsset::register($this);
                 </div>
 
                 <div class="col-md-2">
-                    <a href="/pdf/tech_svid-3.pdf" target="_blank">
+                    <a href="/pdf/tech-svid-4.pdf" target="_blank">
                         <img src="/images/tech-svid-icon.png"  style='height:90px' alt="">
                         <br>
                         <span>Сертификаты соответствия систем</span>
